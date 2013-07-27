7fe1fa1e9e59a4296fc140495b915b0c2d21af5d
Yaniv cohen <yanivomc@gmail.com>
2013-07-27 16:50:09 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faceintegration::Application.config.secret_token = '0adef4ca883c07a160de366ece921d3d98f7e1b265e7414cae18aec3d063ba5fe1c63ae4e8d827cd336a2dd5b0a3a9de367859fdd22b0302b38582577441284e'
